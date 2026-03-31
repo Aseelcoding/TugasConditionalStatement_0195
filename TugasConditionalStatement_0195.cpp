d552b7087a721e742203bffa0dfbe4d89955fb97
Aseel <asilalhashmi@email.com>
2026-03-31 10:54:40 +0700

    add the function getStatus to get string variable and this contain the status

--- TugasConditionalStatement_0195.cpp
+++ TugasConditionalStatement_0195.cpp
@@ -13,7 +13,21 @@ float calculateBMI(float weight, float height)
     float Result= weight / (height * height);
     return Result;
 }
-
+// 3. Function to determine the body condition status
+string getStatus(float bmi) {
+    if (bmi < 18.5) {
+        return "Underweight";
+    }
+    else if (bmi < 25) {
+        return "Normal Weight";
+    }
+    else if (bmi < 30) {
+        return "Overweight";
+    }
+    else {
+        return "Obese";
+    }
+}
 
 int main()
 {
