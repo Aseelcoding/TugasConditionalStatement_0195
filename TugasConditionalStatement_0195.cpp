c8fa61182daf3e65a06953f8af4d56cc188b4fdb
Aseel <asilalhashmi@email.com>
2026-03-31 10:46:03 +0700

    add the first function to get the data from the user

---
+++ TugasConditionalStatement_0195.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+using namespace std;
+void inputData(float& weight, float& height) {
+    cout << "Enter weight (kg): ";
+    cin >> weight;
+    cout << "Enter height (meter): ";
+    cin >> height;
+}
+int main()
+{
+    std::cout << "Hello World!\n";
+}
